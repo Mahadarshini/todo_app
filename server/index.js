a401e462b90ec99199eec286605be78a370d63d4
Maha-kg <mahadarshini290306@gmail.com>
2026-02-19 09:44:19 +0530

    added PORT

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ import pool from "./db.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.get("/", (req, res) => {
   res.send("Server running");
